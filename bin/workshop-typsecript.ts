3d0e4218947bed78d7e65c327edacd41f74e8c86
Nakul Talwar <nakul47@gmail.com>
2021-01-31 18:43:56 -0600

    Added CDK project files

--- bin/workshop-typsecript.ts
+++ bin/workshop-typsecript.ts
@@ -2,5 +2,6 @@
 import * as cdk from '@aws-cdk/core';
 import { WorkshopTypsecriptStack } from '../lib/workshop-typsecript-stack';
 
+const env = { account: '207026233209', region: 'us-east-1'};
 const app = new cdk.App();
-new WorkshopTypsecriptStack(app, 'WorkshopTypsecriptStack');
+new WorkshopTypsecriptStack(app, 'WorkshopTypsecriptStack', {env: env});
--- lib/workshop-typsecript-stack.ts
+++ lib/workshop-typsecript-stack.ts
@@ -2,6 +2,8 @@ import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class WorkshopTypsecriptStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -12,6 +14,14 @@ export class WorkshopTypsecriptStack extends cdk.Stack {
     });
 
     const topic = new sns.Topic(this, 'WorkshopTypsecriptTopic');
+
+    const bucket = new s3.Bucket(this, 'WorkshopBucket', {versioned: true});
+
+    const instance = new ec2.Instance(this, 'WorkshopEC2', {
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.LookupMachineImage({name: 'amzn2-ami-hvm-2.0.20210126.0-x86_64-gp2'}),
+      vpc: ec2.Vpc.fromLookup(this,'WorkshopVPC',{isDefault: true})
+    });
 
     topic.addSubscription(new subs.SqsSubscription(queue));
   }
